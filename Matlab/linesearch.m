96dbd41057299aca952f5802f6e3a1782a23fa8d
Adam Jalkemo <adam@jalkemo.se>
2017-01-18 17:35:56 +0100

    bfgs

--- Matlab/linesearch.m
+++ Matlab/linesearch.m
@@ -1,12 +1,12 @@
 function [lambda,No_of_iterations] = linesearch(func,x,d)
     a = 0;
-    b = find_end_interval(func, x, d)
+    b = find_end_interval(func, x, d);
     alpha = (sqrt(5)-1)/2;
     lambda = a + (1-alpha)*(b-a);
     mu = a + alpha*(b-a);
     f = @(y) func(x+d.*y);
     No_of_iterations = 0;
-    tol = 1e-60;
+    tol = 1e-90;
     fl = f(lambda);
     fm = f(mu);
     while abs(fl-fm) > tol
@@ -30,7 +30,7 @@ function [lambda,No_of_iterations] = linesearch(func,x,d)
     % end
 end
 function b = find_end_interval(func, x, d)
-    b = 1;
+    b = 1e100;
     while isinf(func(x+b*d))
         b = b / 2;
     end
--- Matlab/test_line_search.m
+++ Matlab/test_line_search.m
@@ -2,4 +2,7 @@ tic
 lambda_1=linesearch(@test_func,[0;0],[1;0])
 test_func([0;0] + lambda_1*[1;0])
 toc
-%lambda_2=linesearch(@test_func,[0;0],[0;1]);
+tic
+lambda_2=linesearch(@test_func,[0;0],[0;1])
+test_func([0;0] + lambda_2*[0;1])
+toc
