b94aecf5c5d89bae135be40bb644d95a5dfceaa0
emil@westenius.se <emil@westenius.se>
2017-01-20 13:26:20 +0100

    startpoints as vector

--- Matlab/solver.m
+++ Matlab/solver.m
@@ -1,6 +1,6 @@
 function [y, fy] = solver(f, alpha, eta, start, method, tol, printout)
     for i = 1:length(eta)
        q = @(y) f(y) + eta(i) * alpha(y);
-       [y, fy] = nonlinearmin(q,start,method,tol,printout);
+       [y, fy] = nonlinearmin(q,start(i),method,tol,printout);
     end
 end
