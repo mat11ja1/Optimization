02b950e30600ce0168487f5d2a36ca0efc6aa249
emil@westenius.se <emil@westenius.se>
2017-01-20 14:03:21 +0100

    ex313

--- Matlab/ex313.m
+++ Matlab/ex313.m
@@ -1,9 +1,10 @@
-
+a =@(x) max(0,(sum(x.^2)-10))^2+max(0,(x(2)*x(3)-5*x(4)*x(5)))^2+max(0,(x(1)^3+x(3)^3+1))^2;
+f =@(x) exp(prod(x));
 start = [-2;2;2;-1;-1];
-tol = 1e-10;
+tol = 1e-6;
 eta = [0.01,0.1,1,10,100];
 
-solver(@func313,@alfa313,eta,start,'DFP',tol,1)
+[y,fy] = solver(f,a,eta,start,'BFGS',tol,1)
 
 
 
