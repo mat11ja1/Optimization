18c0a7569e85eb0db02cb456f36ea5e461ac9e3d
Adam Jalkemo <adam@jalkemo.se>
2017-01-20 13:36:05 +0100

    fixed error in solver

--- Matlab/solver.m
+++ Matlab/solver.m
@@ -1,6 +1,7 @@
 function [y, fy] = solver(f, alpha, eta, start, method, tol, printout)
+    y = start;
     for i = 1:length(eta)
        q = @(y) f(y) + eta(i) * alpha(y);
-       [y, fy] = nonlinearmin(q,start,method,tol,printout);
+       [y, fy] = nonlinearmin(q,y,method,tol,printout);
     end
 end
