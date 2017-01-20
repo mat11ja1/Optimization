d9fd5e266691e86b6490b1b9aa4e04414f9f30a7
Adam Jalkemo <adam@jalkemo.se>
2017-01-20 13:59:57 +0100

    9.3 works

--- Matlab/problem9_3.m
+++ Matlab/problem9_3.m
@@ -1,9 +1,9 @@
 f = @(x) exp(x(1)) + x(1)^2 + x(1)*x(2);
 p = @(x) (1/2*x(1) + x(2) - 1).^2;
 
-%f([1;2]) + b([1;2])
-
 x = [0; 0];
-mu = [4, 3, 2];
-%nonlinearmin(f,start,method,tol,printout)
-[y, fy] = solver(f, p, mu, x, 'DFP', 1e-6, 1)
+mu = 2.^(1:5:20);
+[y, fy] = solver(f, p, mu, x, 'DFP', 1e-6, 0)
+fprintf('Found point y: (%0.3f, %0.3f)\n', y(1), y(2))
+fprintf('Minimum value: %0.3f\n', f(y)) 
+fprintf('Penalty at y: %2.2E\n', p(y))
--- Matlab/solver.m
+++ Matlab/solver.m
@@ -3,5 +3,7 @@ function [y, fy] = solver(f, alpha, eta, start, method, tol, printout)
     for i = 1:length(eta)
        q = @(y) f(y) + eta(i) * alpha(y);
        [y, fy] = nonlinearmin(q,y,method,tol,printout);
-    end
+       fprintf('Penalty/Barrier: %2.2E, function value: %2.2E\n', eta(i), fy)
+    end    
+    fy = f(y);
 end
